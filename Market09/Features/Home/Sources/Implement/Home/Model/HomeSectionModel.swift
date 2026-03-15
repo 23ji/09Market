a1400dade594e94a02f1e1dbe3aae7fb626ee4cd
sangjin-hash <sangjin970301@gmail.com>
2026-03-15 23:39:04 +0900

    [FEAT] HomeSectionModel에 AnimatableSectionModelType 적용

--- Market09/Features/Home/Sources/Implement/Home/Model/HomeSectionModel.swift
+++ Market09/Features/Home/Sources/Implement/Home/Model/HomeSectionModel.swift
@@ -11,10 +11,41 @@ import RxDataSources
 
 // MARK: - Section Item
 
-enum HomeSectionItem {
-    case category(GroupBuyingCategory, Bool)
+enum HomeSectionItem: IdentifiableType, Equatable {
+    case category(GroupBuyingCategory?, Bool)
     case top10Banner
-    case post
+    case post(Post)
+
+    var identity: String {
+        switch self {
+        case .category(let category, _):
+            return "category_\(category?.rawValue ?? "all")"
+
+        case .top10Banner:
+            return "top10Banner"
+
+        case .post(let post):
+            return "post_\(post.id)"
+        }
+    }
+
+    static func == (lhs: HomeSectionItem, rhs: HomeSectionItem) -> Bool {
+        switch (lhs, rhs) {
+        case (.category(let lCat, let lSel), .category(let rCat, let rSel)):
+            return lCat == rCat && lSel == rSel
+
+        case (.top10Banner, .top10Banner):
+            return true
+
+        case (.post(let lPost), .post(let rPost)):
+            return lPost.id == rPost.id
+                && lPost.likesCount == rPost.likesCount
+                && lPost.isLiked == rPost.isLiked
+
+        default:
+            return false
+        }
+    }
 }
 
 
@@ -26,30 +57,43 @@ enum HomeSectionModel {
     case postList(items: [HomeSectionItem])
 }
 
-extension HomeSectionModel: SectionModelType {
+extension HomeSectionModel: AnimatableSectionModelType {
     typealias Item = HomeSectionItem
-    
+
+    var identity: String {
+        switch self {
+        case .category:
+            return "category"
+
+        case .top10Banner:
+            return "top10Banner"
+
+        case .postList:
+            return "postList"
+        }
+    }
+
     var items: [HomeSectionItem] {
         switch self {
         case .category(let items):
             return items
-            
+
         case .top10Banner(let items):
             return items
-            
+
         case .postList(let items):
             return items
         }
     }
-    
+
     init(original: HomeSectionModel, items: [HomeSectionItem]) {
         switch original {
         case .category:
             self = .category(items: items)
-            
+
         case .top10Banner:
             self = .top10Banner(items: items)
-            
+
         case .postList:
             self = .postList(items: items)
         }
